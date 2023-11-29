7cb0fdd692de3569109fce55eeb2f2d99ccc41ca
AtharvaSachan123 <atharvasachan823@gmail.com>
2023-11-30 00:33:22 +0530

    Added styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,16 @@ const Home = () => {
     </div>
     <div className="home2">
       <img src={vg} alt='Graphics'/>
+      <div>
+      <p>
+        We are your one and only solution to the tech problems
+        you face
+        everyday.We are loading tech company whose aim is to increase the 
+        problem solving ability in children.
+      </p>
+      </div>
     </div>
+   
     </>
   )
 }
