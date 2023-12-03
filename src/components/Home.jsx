32453307b5df432916607eb21f8860113ffc7a22
AtharvaSachan123 <atharvasachan823@gmail.com>
2023-12-03 18:44:38 +0530

    Added brand section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import vg from '../assests/2.webp'
+import{AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from "react-icons/ai"
 const Home = () => {
   return (
     <>
@@ -21,14 +22,47 @@ const Home = () => {
       </p>
       </div>
     </div>
-    <div className="home3"><div>
+    <div className="home3" id='about'><div>
       <h1>Who we are</h1>
       <p>
       TechyStar is more than a brand; we're a dynamic team of tech enthusiasts dedicated to innovative solutions. Fueled by passion, integrity, and a commitment to excellence, we thrive on pushing boundaries and shaping a future where technology transforms lives. TechyStar: Redefining Tomorrow.
       </p>
     </div>
     </div>
-   
+   <div className="home4" id='brands'>
+    <div>
+      <h1>Brands</h1>
+      <article>
+      <div style={{
+        animationDelay:"0.3s",
+      }}>
+        <AiFillGoogleCircle/>
+        <p>Google</p>
+      </div>
+
+      <div style={{
+        animationDelay:"0.5s",
+      }}>
+        <AiFillAmazonCircle/>
+        <p>Amazon</p>
+      </div>
+
+      <div style={{
+        animationDelay:"0.7s",
+      }}>
+        <AiFillYoutube/>
+        <p>Youtube</p>
+      </div>
+
+      <div style={{
+        animationDelay:"0.3s",
+      }}>
+        <AiFillInstagram/>
+        <p>Instagram</p>
+      </div>
+      </article>
+    </div>
+   </div>
     </>
   )
 }
